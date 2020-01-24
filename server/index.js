47d59d99eef703d4ff1099ae38982cace6ce3ce8
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-01-23 17:12:23 -0700

    Completed

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const checkForSession = require('./middlewares/checkForSession');
 const swagController = require('./controllers/swagController');
 const authController = require('./controllers/authController');
+const cartController = require('./controllers/cartController');
+const searchController = require('./controllers/searchController');
 
 const app = express();
 
@@ -16,11 +18,16 @@ app.use(session({
   saveUninitialized: true
 }));
 app.use(checkForSession);
+app.use(express.static(`${__dirname}/../build`));
 
 app.get('/api/swag', swagController.read);
 app.post('/api/login', authController.login);
 app.post('/api/register', authController.register);
 app.post('/api/signout', authController.signout);
 app.get('/api/user', authController.getUser);
+app.post('/api/cart/checkout', cartController.checkout);
+app.post('/api/cart/:id', cartController.add);
+app.delete('/api/cart/:id', cartController.delete);
+app.get('/api/search', searchController.search);
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
